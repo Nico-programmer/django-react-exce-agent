3f67e74626a870d262608f52b86616b39c990ed4
Nicolas <nicolas.paulo.vega06@gmail.com>
2025-07-16 09:02:16 -0500

    Beta 1.0
    Create a model and database

--- backend/excel_agent/models.py
+++ backend/excel_agent/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# Excel_agent model
+class ExcelTask(models.Model):
+    # Fields
+    instruction = models.TextField(verbose_name='Instrucción')
+    input_file = models.FileField(upload_to='input_excels/', verbose_name='Archivo de entrada')
+    output_file = models.FileField(upload_to='output_excels/', null=True, blank=True, verbose_name='Archivo de salida')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    
+    def __str__(self):
+        return f'{self.pk} - {self.created_at}'
+    
+    class Meta:
+        verbose_name = 'Documento Excel'
+        verbose_name_plural = 'Documentos Excel'
